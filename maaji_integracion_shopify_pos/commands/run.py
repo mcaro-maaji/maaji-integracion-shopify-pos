b7c541795998defdf3dd9e524a1746b1138023d9
Manuel Caro <manuelcaro732@hotmail.com>
2024-12-04 08:12:42 -0500

    Fix: correcion de fechas en el comando run purchase-orders

--- maaji_integracion_shopify_pos/commands/run.py
+++ maaji_integracion_shopify_pos/commands/run.py
@@ -3,22 +3,62 @@
 from datetime import datetime, timedelta
 import click
 from ..controllers import purchase_order
-from ..config import KeySitesShopifyStores
+from ..config import key_sites_shopify_stores as key_stores
+
+dt_formats = ("%d/%m/%Y", "%d/%m/%YT%H:%M:%S", "%d/%m/%Y %H:%M:%S",
+                    "%Y-%m-%d", "%Y-%m-%dT%H:%M:%S", "%Y-%m-%d %H:%M:%S", "%H:%M:%S")
+
+class DateOrDeltaTime(click.DateTime):
+    """Crea un tipo de dato timedelta o datetime como parametro de CLI."""
+    name = '[datetime | timedelta]'
+
+    def convert(self, value: str | datetime | timedelta, param, ctx):
+        if isinstance(value, (datetime, timedelta)):
+            return value
+        try:
+            # Format 1: # weeks, # days, hours:minutes:seconds:microseconds:milliseconds
+            # Format 2: # days, hours:minutes:seconds:microseconds:milliseconds
+            value_parts = value.split(", ")
+            time_parts = value_parts.pop()
+            if len(value_parts) == 2:
+                [weeks_part, days_part] = value_parts
+            elif len(value_parts) == 1:
+                [weeks_part, days_part] = (None, value_parts[0])
+            else:
+                [weeks_part, days_part] = (None, None)
+
+            num_weeks = 0. if weeks_part is None else float(weeks_part.replace(" weeks", ""))
+            num_days = 0. if days_part is None else float(days_part.replace(" days", ""))
+            num_times = map(float, time_parts.split(":"))
+            keys = ("weeks", "days", "hours", "minutes", "seconds", "microseconds", "milliseconds")
+            items = zip(keys, [num_weeks, num_days, *num_times])
+            kwargs = {}
+            kwargs.update(items)
+            return timedelta(**kwargs)
+        except ValueError:
+            return super().convert(value, param, ctx)
 
 @click.command("purchase-orders")
-@click.option("--store", default="maaji_co_test", help="seleccione la tienda shopify.")
-@click.option("--date", help="Fecha del día para buscar las facturas en el servicio D365.")
-@click.option("--env", default="prod", help="Entorno de Dynamics 365. [prod | uat]")
-@click.option("--days", default=1, help="Cantidad de días posteriores a la fecha --date.")
-def run_purchase_orders(store: KeySitesShopifyStores, env, date, days):
-    """Comando para ejecutar el servicio de crear ordenes de compra mediante D365 y Stocky"""
-    date_start = datetime.strptime(date, "%d/%m/%Y")
-    date_end = date_start + timedelta(days, seconds=-1)
+@click.option("-s", "--store", type=click.Choice(key_stores), required=True)
+@click.option("-e", "--env", type=click.Choice(("uat", "prod")), default="prod")
+@click.option("-d", "--date-end", "--date", type=click.DateTime(dt_formats), default=datetime.now())
+@click.option("-ts", "-ds", "--time-start", "--date-start", type=DateOrDeltaTime(dt_formats))
+def run_purchase_orders(store, env, date_end: datetime, time_start):
+    """Ejecuta el servicio de crear ordenes de compra Stocky mediante el servicio D365."""
+    if time_start is None:
+        time_start = date_end.replace(hour=0, minute=0, second=0, microsecond=0)
+    if isinstance(time_start, timedelta):
+        date_start = date_end + time_start
+    elif isinstance(time_start, datetime):
+        date_start = time_start
+    else:
+        raise ValueError("No se ha establecido la fecha de inicio '--date-start'.")
+
     payload = purchase_order.DataApiPayload("AM", date_start, date_end)
     purchase_order.create_from_service(payload, store, env)
 
 @click.group()
 def run():
-    """Comando para ejecutar catacteristicas de la integración."""
+    """Ejecutar catacteristicas de la integración."""
 
 run.add_command(run_purchase_orders)
