761a11cd498a540cb26b548351ec52c1186efc4c
Manuel Caro <manuelcaro732@hotmail.com>
2024-12-18 07:24:10 -0500

    Feature: añadir forma de desplegar cambios automaticamente

---
+++ extra/task_cd.py
@@ -0,0 +1,9 @@
+"""
+Modulo que ejecuta tareas de Distribución Continua o CD en producción.
+Es util a la hora de realizar cambios en archivos o ejecutar comandos post-despliegue del proyecto.
+"""
+
+from maaji_integracion_shopify_pos.config import Configuration
+
+Configuration.purchase_orders.default_supplier_name_like = "ART MODE"
+Configuration.save_file()
--- maaji_integracion_shopify_pos/__init__.py
+++ maaji_integracion_shopify_pos/__init__.py
@@ -1,6 +1,1 @@
 """TODO: DOCS"""
-
-__all__ = ["main", "cli"]
-
-from .main import main
-from .cli import cli
